e48320e992dc753bda429fefec3e8437365f26ff
TanuhaSerkova <89786945+TanuhaSerkova@users.noreply.github.com>
2022-07-14 18:55:37 +0300

    +

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 
 const styles = {
     container: {
@@ -32,13 +32,6 @@ const HomePage = () => (
     <div style={styles.container}>
         <h1 style={styles.title}>PhoneBook☎️servise</h1>
         <span style={styles.text}>SignIn</span>
-        <Link to="/register" style={styles.link}>
-        Don't have an account?
-        </Link>
-
-        <Link to="/login" style={styles.link}>
-        Already have an account?
-        </Link>
     </div>
 );
 
